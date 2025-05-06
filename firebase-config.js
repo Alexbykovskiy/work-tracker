3d199307df0195cfc397c994c4f13a1b1a9118e1
Alexbykovskiy <Alexbykovskiytattoo@gmail.com>
2025-05-06 09:30:02 +0200

    1

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,14 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Конфигурация Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyCiPP4AYEfu7E14Kk_CVxqioaf5QX5rz70",
+  apiKey: "AIzaSyCiPPAAYEfu7E14kK_CVxqioaf5QX5rz70",
   authDomain: "work-tracker-50eab.firebaseapp.com",
   projectId: "work-tracker-50eab",
-  storageBucket: "work-tracker-50eab.firebasestorage.app",
+  storageBucket: "work-tracker-50eab.appspot.com",
   messagingSenderId: "397564235584",
   appId: "1:397564235584:web:cb19ad8d41eb3c7a8ff4a5",
   measurementId: "G-21W3EGG9JB"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Инициализация Firebase (для версии с Firebase 8)
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();

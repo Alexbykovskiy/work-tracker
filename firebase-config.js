f151ced917b9c1378f70a3573a62d0327c29ab5b
Alexbykovskiy <Alexbykovskiytattoo@gmail.com>
2025-05-06 09:37:12 +0200

    1

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,3 @@
-// Конфигурация Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyCiPPAAYEfu7E14kK_CVxqioaf5QX5rz70",
   authDomain: "work-tracker-50eab.firebaseapp.com",
@@ -8,7 +7,5 @@ const firebaseConfig = {
   appId: "1:397564235584:web:cb19ad8d41eb3c7a8ff4a5",
   measurementId: "G-21W3EGG9JB"
 };
-
-// Инициализация Firebase (для версии с Firebase 8)
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
